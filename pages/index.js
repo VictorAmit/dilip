b05995651db952aa9d8c6409e63e1b6b25a73aa5
Victor Amit <44795913+VictorAmit@users.noreply.github.com>
2021-09-24 16:47:04 +0530

    6

--- pages/index.js
+++ pages/index.js
@@ -75,11 +75,8 @@ export default function Home() {
 
 								<h4 className="header_title"> I Do <span id="typed"></span></h4>
 								<p className="short-info">
-									With over 5+ years of job experience.
-									I studied Information Technology at University and later honed my skills through commercial and
-									freelance experience. My skills include PHP, Laravel, jQuery, database design, JavaScript and MySQL.
-								</p>
-								<a id="g-p-f-h" className="pagelink mybtn mybtn-bg" href="#"><span><i
+									With over 5+ years of job experience. I am work as a Social Media Hander and later I'm hounrable Member of Bjp Delhi. My skills include A Leader , An Activist , Social Worker & Media Representative.	</p>
+								<a id="g-p-f-h" className="pagelink mybtn mybtn-bg" target="_blank" href="https://twitter.com/dilipkumar999"><span><i
 											className="fas fa-briefcase"></i>Portfolio</span></a>
 							</div>
 						</div>
